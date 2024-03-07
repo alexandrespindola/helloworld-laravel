ed22e3b40f2649b6f0876de03aa7659648715787
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-07 17:43:23 +0100

    Atualiza rotas e remove página de sobre

--- resources/views/landing/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'About')
-
-@section('content')
-<h1 class="bg-red-700 text-white">LANDING</h1>
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::view('/', 'index')->name('index');
-Route::view('/about', 'landing.about')->name('about');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/services', 'pages.services')->name('services');
+Route::view('/contact', 'pages.contact')->name('contact');
