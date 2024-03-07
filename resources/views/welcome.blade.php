8ef7a472392d71079ae2a662c45c51e5f8a78169
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-07 17:19:47 +0100

    Adiciona pacote Livewire e atualiza página inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@section('title', 'Hello World 8')
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
-
-</head>
-
-<body>
+@section('content')
     <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello 5</h1>
-    <p class="bg-green-300 text-orange-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed itaque ipsa esse excepturi molestias libero? Magni laboriosam dolorum deserunt explicabo et. Nobis sunt dolorem quidem laudantium nemo facere quaerat itaque.
-    Atque doloribus ut vel corporis temporibus expedita dolor sit culpa alias, officia error beatae aperiam iste ratione cumque eveniet? Laborum inventore quos ab iure, porro culpa voluptatem tempora cumque laboriosam.
-    Nisi corrupti porro magni fuga deleniti optio nihil necessitatibus, perferendis expedita modi quaerat consequatur cum reprehenderit facere dignissimos ipsa similique tempore doloribus! Nisi amet quaerat unde perspiciatis odio quo voluptates.</p>
-</body>
+    <div x-data="{ open: false }">
+        <button @click="open = !open" class="bg-blue-400 rounded-md p-2 text-white">Toggle</button>
 
-</html>
+        <div x-show="open">
+            Olá, Alpine.js!
+        </div>
+    </div>
+
+@endsection
