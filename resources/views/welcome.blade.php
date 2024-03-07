82517a1620eb8ab8e73972260052ab3602ed3fa7
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-07 16:58:41 +0100

    git init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello 5</h1>
+    <p class="bg-green-300 text-orange-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed itaque ipsa esse excepturi molestias libero? Magni laboriosam dolorum deserunt explicabo et. Nobis sunt dolorem quidem laudantium nemo facere quaerat itaque.
+    Atque doloribus ut vel corporis temporibus expedita dolor sit culpa alias, officia error beatae aperiam iste ratione cumque eveniet? Laborum inventore quos ab iure, porro culpa voluptatem tempora cumque laboriosam.
+    Nisi corrupti porro magni fuga deleniti optio nihil necessitatibus, perferendis expedita modi quaerat consequatur cum reprehenderit facere dignissimos ipsa similique tempore doloribus! Nisi amet quaerat unde perspiciatis odio quo voluptates.</p>
+</body>
+
+</html>
