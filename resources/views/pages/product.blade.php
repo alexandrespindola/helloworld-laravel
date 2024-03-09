7661bd775cff1ab69aa62012f3b424068b022088
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-09 12:16:54 +0100

    Atualiza rotas: adiciona controlador de produtos e remove comentários desnecessários

---
+++ resources/views/pages/product.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Product')
+
+@section('content')
+    <h1 class="bg-yellow-400 text-blue-700 text-4xl">Product</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\NoteController;
-// use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'pages.about')->name('about');
 Route::view('/services', 'pages.services')->name('services');
 Route::view('/contact', 'pages.contact')->name('contact');
-
 Route::get('/users', [UserController::class, 'index'])->name('users');
 Route::get('/create', [UserController::class, 'create'])->name('user.create');
 
-// Route::get('/note/{id?}', [NoteController::class, 'index'])->name('note.index');
-
 Route::resource('note', NoteController::class);
 
-// Route::get('/note', [NoteController::class, 'index'])->name('note.index');
-// Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
-// Route::post('/note/store', [NoteController::class, 'store'])->name('note.store');
-// Route::get('/note/edit/{note}', [NoteController::class, 'edit'])->name('note.edit');
-// Route::put('/note/update/{note}', [NoteController::class, 'update'])->name('note.update');
-// Route::get('/note/show/{note}', [NoteController::class, 'show'])->name('note.show');
-// Route::delete('/note/destroy/{note}', [NoteController::class, 'destroy'])->name('note.destroy');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 
-// Route::resource('posts', PostController::class);
+
