489c1e5ccdd6a73fc168c18b4a482bdbf4e488e8
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-11 06:43:37 +0100

    Atualiza título da página inicial para "Hello Laravel com Git"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,5 @@
 @section('title', 'Hello Laravel')
 
 @section('content')
-    <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello Laravel Branch</h1>
+    <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello Laravel with Git</h1>
 @endsection
