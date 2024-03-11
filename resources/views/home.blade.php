f245ac20c9460061568bbc29e206226ecbb7f1c2
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-11 06:16:55 +0100

    Atualiza título da página inicial para "Hello Laravel Branch"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Hello Laravel')
 
 @section('content')
-    <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello Laravel</h1>
+    <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello Laravel Branch</h1>
 
     {{-- <div x-data="{ open: false }">
         <button @click="open = !open" class="bg-blue-400 rounded-md p-2 text-white">Toggle</button>
