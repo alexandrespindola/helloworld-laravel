d100c9f14a3031775b2a70e47e5f05e81199ffd2
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-11 06:30:05 +0100

    Removido código comentado e não utilizado da página inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,4 @@
 
 @section('content')
     <h1 class="bg-yellow-400 text-blue-700 text-4xl">Hello Laravel Branch</h1>
-
-    {{-- <div x-data="{ open: false }">
-        <button @click="open = !open" class="bg-blue-400 rounded-md p-2 text-white">Toggle</button>
-
-        <div x-show="open">
-            Olá, Alpine.js! :D
-        </div>
-    </div> --}}
-
 @endsection
